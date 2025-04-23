951427e215217eb3b65df26a01213a7a33364149
Iker-kun <ikerjuma@ucm.es>
2025-04-23 18:21:20 +0200

    Color añil al tie, faltas wings

--- Practica/IG1App/AdvancedTIE.cpp
+++ Practica/IG1App/AdvancedTIE.cpp
@@ -13,23 +13,26 @@ AdvancedTIE::AdvancedTIE()
 {
 	//Radio, paralelos, meridianos
 	Sphere* sphere = new Sphere(100.0f);
+	sphere->setColor({ 0.0f,0.25f,0.41f,1.0f }); //(0,65,106)
 	addEntity(sphere);
 
 	//Altura / Radio inferior / Radio superior
 	Cone* coneA = new Cone(300.0f, 20.0f, 20.0f);
+	coneA->setColor({ 0.0f,0.25f,0.41f,1.0f }); //(0,65,106)
 	coneA->setModelMat(rotate(mat4(1.0f), radians(45.0f), vec3(0.0f, 0.0f, 1.0f)));
 	//coneA->setModelMat(translate(coneA->modelMat(), vec3(-15.0f, 40.0f, 0.0f)));
 	addEntity(coneA);
 
 	//Radio exterior / Radio interior / nRings / nSamples
 	Disk* disk = new Disk(32.0f, 0.0f, 20, 20);
+	disk->setColor({ 0.0f,0.25f,0.41f,1.0f }); //(0,65,106)
 	mat4 trasDisk = translate(disk->modelMat(), vec3(0.0f, 75.0f, 30.0f));
 	mat4 rotDisk = rotate(disk->modelMat(), radians(45.0f), vec3(1.0f, 0.0f, 0.0f));
 	disk->setModelMat(disk->modelMat() * rotDisk * trasDisk);
 	addEntity(disk);
 
 	Cone* coneB = new Cone(100.0f, 30.0f, 30.0f);
-	coneB->setColor({ 0.0f,0.0f,0.0f,1.0f });
+	coneB->setColor({ 0.0f,0.25f,0.41f,1.0f }); //(0,65,106)
 	mat4 trasconeB = translate(coneB->modelMat(), vec3(0.0f, 50.0f,20.0f));
 	mat4 rotconeB = rotate(coneB->modelMat(), radians(45.0f), vec3(1.0f, 0.0f, 0.0f));
 	coneB->setModelMat(coneB->modelMat() * rotconeB * trasconeB);
