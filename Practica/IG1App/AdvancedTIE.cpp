0f00b7ea68a5dd8ab89ab7e3cf6be0aff02a9c66
amielrm4 <amielr01@ucm.es>
2025-04-23 15:52:54 +0200

    lo mejor que he conseguido

--- Practica/IG1App/AdvancedTIE.cpp
+++ Practica/IG1App/AdvancedTIE.cpp
@@ -12,17 +12,17 @@ using namespace glm;
 AdvancedTIE::AdvancedTIE()
 {
 	//Radio, paralelos, meridianos
-	Sphere* sphere = new Sphere(10.0f);
+	Sphere* sphere = new Sphere(100.0f);
 	addEntity(sphere);
 
 	//Altura / Radio inferior / Radio superior
-	Cone* coneIz = new Cone(100.0f, 10.0f, 10.0f);
-	mat4 tras = translate(mat4(1.0f), vec3(-100.0f, 0.0f, 0.0f));
-	mat4 rot = rotate(tras, radians(45.0f), vec3(0.0f, 0.0f, 1.0f));
-	coneIz->setModelMat(rot);
-	addEntity(coneIz);
+	Cone* coneA = new Cone(300.0f, 20.0f, 20.0f);
+	coneA->setModelMat(rotate(mat4(1.0f), radians(45.0f), vec3(0.0f, 0.0f, 1.0f)));
+	addEntity(coneA);
 
-
-	Cone* coneDr = new Cone(100.0f, 10.0f, 10.0f);
-	//addEntity(coneDr);
+	Cone* coneB = new Cone(100.0f, 30.0f, 30.0f);
+	coneB->setModelMat(rotate(mat4(1.0f), radians(45.0f), vec3(1.0f, 0.0f, 0.0f)));
+	coneB->setModelMat(translate(coneB->modelMat(), vec3(0.0f, 0.0f, 50.0f)));
+	//coneB->setModelMat(translate(coneB->modelMat(), vec3(0.0f, 20.0f, 0.0f)));
+	addEntity(coneB);
 }
