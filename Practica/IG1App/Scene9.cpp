e77f8c6c67a0cadba6dcbbc80b565f87c73775be
amielrm4 <amielr01@ucm.es>
2025-04-30 15:58:22 +0200

    Escena 9 sin materiales

--- Practica/IG1App/Scene9.cpp
+++ Practica/IG1App/Scene9.cpp
@@ -1,5 +1,6 @@
 #include "Scene9.h"
 
+#include "Sphere.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
@@ -16,5 +17,15 @@ void Scene9::init()
 
 	// Graphics objects (entities) of the scene
 	gObjects.push_back(new RGBAxes(400.0f));
+
+	Sphere* tat1 = new Sphere(100.0f, 40, 40);
+	tat1->setMaterial(new Material({ 1, 1, 0 }));
+	tat1->setModelMat(translate(mat4(1), vec3(-150, 0, 0)));
+	gObjects.push_back(tat1);
+
+	Sphere* tat2 = new Sphere(100.0f, 40, 40);
+	tat2->setMaterial(new Material({ 1, 1, 0 }));
+	tat2->setModelMat(translate(mat4(1), vec3(150, 0, 0)));
+	gObjects.push_back(tat2);
 
 }
